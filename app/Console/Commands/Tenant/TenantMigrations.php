07cb2e6092f556ec2f7bc779c130a2203d09dcf2
jhomoreira <jho@outlook.com>
2021-10-15 17:26:06 -0300

    adicionado refresh migration para novas colunas de tabelas

--- app/Console/Commands/Tenant/TenantMigrations.php
+++ app/Console/Commands/Tenant/TenantMigrations.php
@@ -14,7 +14,7 @@ class TenantMigrations extends Command
      *
      * @var string
      */
-    protected $signature = 'tenants:migrations';
+    protected $signature = 'tenants:migrations {--refresh}';
 
     /**
      * The console command description.
@@ -43,6 +43,8 @@ class TenantMigrations extends Command
      */
     public function handle()
     {
+        $command = $this->option('refresh') ? 'migrate:refresh' : 'migrate';
+
         $companies = Company::all();
 
         foreach($companies as $company){
@@ -51,7 +53,7 @@ class TenantMigrations extends Command
 
             $this->info("Connecting Company {$company->name}");
 
-            Artisan::call('migrate', [
+            Artisan::call($command, [
                 '--force' => true,
                 '--path' => '/database/migrations/tenant',
             ]);
